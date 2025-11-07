0ef71d349360906761703c64a755f85956aa4560
abhibs <abhirambs97@gmail.com>
2025-11-07 16:30:46 +0530

    creating first home route

--- proj1/index.js
+++ proj1/index.js
@@ -2,6 +2,10 @@ var express = require('express')
 
 app = express()
 
+app.get('/', function (req, res) {
+  res.send('Abhiram B S')
+})
+
 app.listen(8000, function () {
   console.log('Server Is Running')
 })
